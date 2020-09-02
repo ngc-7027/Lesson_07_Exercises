fffc9b697eee139fdf995276b2d33f07e4354947
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-02 23:43:13 +0200

    Přidání výpočtu věku hráče do třídy ChessPlayer

--- Lesson_07_Exercises.App/ChessPlayer.cs
+++ Lesson_07_Exercises.App/ChessPlayer.cs
@@ -7,7 +7,20 @@ namespace Lesson_07_Exercises.App
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime Birthdate { get; set; }
-        public int AgeInYears { get; set; }
+        public int AgeInYears
+        {
+            get
+            {
+                var age = DateTime.Now.Year - Birthdate.Year;
+
+                if (Birthdate.Date > (DateTime.Now.AddYears(-age).Date))
+                {
+                    age--;
+                }
+
+                return age;
+            }
+        }
         public int EloRating { get; set; }
         public TimeSpan ThisYearTrainingTime { get; set; }
         public Country Country { get; set; }
